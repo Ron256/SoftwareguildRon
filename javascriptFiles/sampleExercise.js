5cc86ef32bd3986a95fd6b49e2b527d4eb917c02
Ron256 <ejaluronaldlee@gmail.com>
2017-03-03 23:25:39 -0500

    modified the  SampleExercise.js

--- javascriptFiles/sampleExercise.js
+++ javascriptFiles/sampleExercise.js
@@ -11,23 +11,26 @@ function clearErrors() {
     }    
 }
 
-function resetForm() {
+function resetForm() 
+{
     clearErrors();
     document.forms["sampleform"]["inputStartingNo"].value = "";
     document.forms["sampleform"]["inputEndingNo"].value = "";
 	document.forms["sampleform"]["inputStepNo"].value = "";
     document.getElementById("results").style.display = "none";
     document.getElementById("submitButton").innerText = "Display Evens";
-    document.forms["numberFun"]["inputStartingNo"].focus();
+    document.forms["sampleform"]["inputStartingNo"].focus();
 }
 
 
 
-function validateitems() {
+function validateItems() 
+{
 	clearErrors();
 	var inputStartingNo = document.forms["sampleform"]["inputStartingNo"].value;
 	var inputEndingNo   = document.forms["sampleform"]["inputEndingNo"].value;
 	var inputStepNo     = document.forms["sampleform"]["inputStepNo"].value;
+	
 	if (inputStartingNo == "" || isNaN(inputStartingNo)) {
         alert("Starting Number must be filled in with a number.");
         document.forms["sampleform"]["inputStartingNo"]
@@ -44,30 +47,40 @@ function validateitems() {
         return false;
     }
 	
-	/*if (inputStartingNo >= inputEndingNo) {
+	/*if (inputEndingNo  <= inputStartingNo) {
 		alert("Ending Number is less than or equal to the starting number.");
         document.forms["sampleform"]["inputEndingNo"]
            .parentElement.className = "form-group has-error";
         document.forms["sampleform"]["inputEndingNo"].focus();
         return false;
-	}
-	*/
+	}*/
 	
-	if (inputStepNo == "" || isNaN(inputStepNo)) {
+	
+	if (inputStepNo == "" || isNaN(inputStepNo)) 
+	{
         alert("Step must be filled in with a number.");
         document.forms["sampleform"]["inputStepNo"]
            .parentElement.className = "form-group has-error";
         document.forms["sampleform"]["inputStepNo"].focus();
         return false;
     }
-	else if (inputStepNo < 0) {
+	else if (inputStepNo < 0) 
+	{
 		alert("Step is not a positive number");
 		document.forms["sampleform"]["inputStepNo"]
            .parentElement.className = "form-group has-error";
         document.forms["sampleform"]["inputStepNo"].focus();
         return false;
 	}
+	document.getElementById("submitButton").innerText = "Redisplay Even nos";
 	
+	/*
+	 function takes the starting, ending and step numbers as parameters and 
+	 constructs a paragraph with the expected  even  numbers
+	 
+	 step 1 construct an an array with the given parameters
+	 step 2 derive another array with even numbers to use the construct the paragaph  
+	*/
 	 function register(inputStartingNo, inputEndingNo, inputStepNo) {
 		var snum    = inputStartingNo; //4
 		var endnum  = inputEndingNo;  //20
@@ -94,52 +107,16 @@ function validateitems() {
 			} 
 		}
 		
-		var constText = "Here are the even numbers between " + snum + "and " + endnum + "by " + step + "'s " + "<br>";
+		var derivedText = "Here are the even numbers between " + inputStartingNo + " and " + inputEndingNo + " by " + step + "'s: ";
 		
-		for(var i = 0; i < evenNos.length; i++) 
+		for (var i = 0; i < evenNos.length; i++) 
 		{
-			constText += evenNos[i] + "<br>"; 
+			derivedText +=  "<br/>" + evenNos[i] + "<br/>"; 
 		}
-		return constText;
+		return derivedText;
 	 }
-	document.getElementById("results").style.display = "block";
+	
 	document.getElementById("results").innerHTML = register(inputStartingNo, inputEndingNo, inputStepNo);
 	
   return false;
 }
-
-
-/*function populateArray(inputStartingNo, inputEndingNo, inputStepNo) {
-	var snum    = inputStartingNo; //4
-	var endnum  = inputEndingNo;  //20
-	var step 	= inputStepNo;    //3
-	
-	var numbers = [];
-	
-	var cnt = 0;
-	
-	while (snum < endnum) { numbers[cnt] = snum; snum += step; cnt++;}
-	//array populated [4, 7, 10, 13, 16, 19]
-	
-	var evenNos = [];
-	
-	for (var i = 0; i < numbers.length; i++) {  
-	if(numbers[i] % 2 === 0) { 
-		evenNos.push(numbers[i]); 
-		} 
-	}
-	
-	var constText = "Here are the even numbers between " + snum + "and " + endnum + "by " + step + "'s " + "<br>";
-	
-	for(var i = 0; i < evenNos.length; i++) {
-		constText += evenNos[i] + "<br>"; 
-		}
-	
-}
-*/
-
-
-
-
-
-	
